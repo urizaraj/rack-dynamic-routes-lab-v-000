f3348cc39e17fa2548704de0b71328950209349e
Alexander Urizar <urizaraj@gmail.com>
2018-02-26 17:24:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,10 @@ class Application
   def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
-    
+    if req.path.match(/items/)
+      resp.write('good')
+    else
+      resp.status = 404
+    end
   end
 end
