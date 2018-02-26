cb19e3b3f5c9a29815eeeed8be801de6e4a8918d
Alexander Urizar <urizaraj@gmail.com>
2018-02-26 17:29:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,14 +3,15 @@ class Application
     Item.new('Figs', 200),
     Item.new('Berries', 400)
   ]
-  
+
   def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
     if req.path.match(/items/)
 
       item = req.path.split(/items/).last
-      
+
+      inst = @@items.find { |e| e.name == item }
       resp.write('good')
     else
       resp.write('Route not found')
