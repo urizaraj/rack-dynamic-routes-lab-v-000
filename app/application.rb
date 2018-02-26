20f952d36e0440d60eaac033b4803a288b10b5ba
Alexander Urizar <urizaraj@gmail.com>
2018-02-26 17:35:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     resp = Rack::Response.new
     if req.path.match(/items/)
 
-      item = req.path.split(/items/).last
+      item_name = req.path.split(/items/).last
 
       inst = @@items.find { |e| e.name == item }
 
