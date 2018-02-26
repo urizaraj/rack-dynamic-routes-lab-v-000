1b1c9ef9e7d79178c6caa84547ce163539121d2d
Alexander Urizar <urizaraj@gmail.com>
2018-02-26 17:35:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,8 +13,8 @@ class Application
 
       item = @@items.find { |i| i.name == item_name }
 
-      if inst
-        resp.write(inst.price)
+      if item
+        resp.write(item.price)
       else
         resp.write('Item not found')
         resp.status = 400
